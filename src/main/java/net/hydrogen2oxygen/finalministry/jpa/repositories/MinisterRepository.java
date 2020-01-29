174f07912cee78f1e48f3d96deb933b50291ddd6
hydrogen2oxygen <luxuspeter@googlemail.com>
2020-01-29 18:45:26 +0100

    findByNameContains

--- src/main/java/net/hydrogen2oxygen/finalministry/jpa/repositories/MinisterRepository.java
+++ src/main/java/net/hydrogen2oxygen/finalministry/jpa/repositories/MinisterRepository.java
@@ -4,10 +4,12 @@ import net.hydrogen2oxygen.finalministry.jpa.Minister;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource(collectionResourceRel = "minister", path = "minister")
 public interface MinisterRepository extends JpaRepository<Minister, UUID> {
 
     Minister findByName(String name);
+    List<Minister> findByNameContains(String text);
 }
