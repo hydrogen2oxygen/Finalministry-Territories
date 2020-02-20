6bd64bf517acbece1271093301a851cd8375273a
hydrogen2oxygen <luxuspeter@googlemail.com>
2020-02-20 19:17:23 +0100

    wip login logout https://spring.io/guides/tutorials/spring-security-and-angular-js/

--- src/main/ui/src/app/app-routing.module.ts
+++ src/main/ui/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {MapComponent} from "./components/map/map.component";
 import {SettingsComponent} from "./components/settings/settings.component";
 import {TerritoriesComponent} from "./components/territories/territories.component";
+import {LoginComponent} from "./components/session/login/login.component";
 
 const routes: Routes = [
   {path: '', component: DashboardComponent},
   {path: 'territories', component: TerritoriesComponent},
   {path: 'map', component: MapComponent},
-  {path: 'settings', component: SettingsComponent}
+  {path: 'settings', component: SettingsComponent},
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/main/ui/src/app/components/menu/menu.component.ts
+++ src/main/ui/src/app/components/menu/menu.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {SessionService} from "../../services/session.service";
+import {take, finalize} from 'rxjs/operators';
+import {environment} from "../../../environments/environment";
+import {BaseUrlUtility} from "../../utilities/BaseUrlUtility";
 
 @Component({
   selector: 'app-menu',
@@ -7,14 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  isMenuCollapsed:boolean=true;
+  isMenuCollapsed: boolean = true;
+  serverUrl:string = `${BaseUrlUtility.getBaseUrl()}:${environment.serverPort}`;
 
-  constructor() { }
+  constructor(private app: SessionService, private http: HttpClient, private router: Router) {
+    this.app.authenticate(undefined, undefined);
+  }
 
   ngOnInit() {
   }
 
   search() {
 
+  }
+
+  logout() {
+    this.http.post(this.serverUrl + '/logout', {}).subscribe(() => {
+      this.app.authenticated = false;
+      this.router.navigateByUrl('/login');
+    });
   }
 }
