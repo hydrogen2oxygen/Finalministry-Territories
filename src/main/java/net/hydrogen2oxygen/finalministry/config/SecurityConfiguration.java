52ed019c518f0c860ac1c0011b2e76d6f6375224
hydrogen2oxygen <luxuspeter@googlemail.com>
2020-02-25 16:35:57 +0100

    css path security

--- src/main/java/net/hydrogen2oxygen/finalministry/config/SecurityConfiguration.java
+++ src/main/java/net/hydrogen2oxygen/finalministry/config/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/index.html", "/", "/home", "/login","/*.js","*.ico").permitAll()
+                .antMatchers("/index.html", "/", "/home", "/login","/*.js","/*.ico","/*.css").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**")
                 .permitAll().anyRequest()
                 .authenticated()
